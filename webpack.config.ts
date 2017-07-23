17e09ea5f5a19048e1e246da9327cb79a42a2543
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-07-24 00:04:47 +0900

    Rename webpack config files

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,37 +1,9 @@
 // tslint:disable:no-var-requires
 // tslint:disable:object-literal-sort-keys
 
-import * as webpack from 'webpack';
 import * as merge from 'webpack-merge';
 import config from './webpack.config.shared';
 
 export default merge(config, {
-  devtool: 'source-map',
-
-  performance: {
-    hints: 'warning',
-  },
-
-  plugins: [
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-      debug: false,
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production'),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      beautify: false,
-      mangle: {
-        screw_ie8: true,
-        keep_fnames: true,
-      },
-      compress: {
-        screw_ie8: true,
-      },
-      comments: false,
-    }),
-  ],
+  devtool: 'eval-source-map',
 });
--- webpack.dev.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable:no-var-requires
-// tslint:disable:object-literal-sort-keys
-
-import * as merge from 'webpack-merge';
-import config from './webpack.config.shared';
-
-export default merge(config, {
-  devtool: 'eval-source-map',
-});
