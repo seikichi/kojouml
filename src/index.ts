3aa7df1b082ad2ed84e3832a9331f0cdc258b2c1
Seiichi KONDO <seikichi@kmc.gr.jp>
2017-07-24 01:38:27 +0900

    Use viz-lite.js instead of viz.js
    After webpack build, lib/index.js becomes 1.8MB

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as diagram from './diagram';
 import * as dot from './dot';
 import * as parser from './parser';
-const Viz: (source: string) => string = require('viz.js');
+const Viz: (source: string) => string = require('viz.js/viz-lite.js');
 
 const Kojo = {
   generateSvg(str: string): string {
